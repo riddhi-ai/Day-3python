931d5457fd5c3fff0f80fbd607250baa0e71d405
Riddhi Naskari <riddhinaskari@gmail.com>
2026-09-10 15:12:24 +0530

    first commit

---
+++ age.py
@@ -0,0 +1,6 @@
+#write a program to print age in days
+#OUTPUT : 3years = 1095 days
+#days=age*365
+
+age = int (input ("Enter age"))
+print(f" {age} years= {age*365} days")
---
+++ oddeven.py
@@ -0,0 +1,4 @@
+a = int (input ("Enter number"))
+b=(a%2!=0)
+print(f"Number is odd: {b}")
+#OUTPUT = Number is odd: false
