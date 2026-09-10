36fae88b87edc5fecc2d1af994ce1be4864229cc
Riddhi Naskari <riddhinaskari@gmail.com>
2026-09-10 15:31:59 +0530

    third commit

---
+++ lastdigit.py
@@ -0,0 +1,7 @@
+#write a program to extract the last digit of an number
+#OUTPUT: 1234; last digit is 4
+
+a= int (input ("Enter an integer"))
+b =a%10
+#print("The last digit of the integer is ,{b}")
+print(f" {a} : last digit is {a%10} ")
---
+++ mintohrs.py
@@ -0,0 +1,5 @@
+#program to convert minutes to hours
+#ex : 135 is 2 hrs 15 min
+
+min= int (input ("Enter Minutes"))
+print(f" {min} minutes= {min//60}hours {min%60} minutes")
