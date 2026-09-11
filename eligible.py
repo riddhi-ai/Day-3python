9d17e20384d9d9fe6d8d5336a6e41f8fed65fd64
Riddhi Naskari <riddhinaskari@gmail.com>
2026-09-11 07:17:26 +0530

    new file

---
+++ eligible.py
@@ -0,0 +1,4 @@
+#role and age varible input values
+#write a program to check if a person is eligible for discount the criteria is he must be student and age must be 21
+#without if else
+#output: Eligible: true
---
+++ swap.py
@@ -0,0 +1,9 @@
+#write a program to swap two variables without third variable and using arothmetic variable
+# example : before swap a= 10, b =20
+ # after swap a =20 , b =10
+a=10
+b=20
+a,b=b,a
+print("After Swapping:")
+print("a:",a)
+print("b:",b)
